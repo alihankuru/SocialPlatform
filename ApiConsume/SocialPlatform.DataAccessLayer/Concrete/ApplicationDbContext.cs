fe769890637b5e05887f2f4bdfd8496f23244146
AK <ali.kuru6@hotmail.com>
2024-01-11 00:07:34 +0300

    Add project files.

---
+++ ApiConsume/SocialPlatform.DataAccessLayer/Concrete/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SocialPlatform.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialPlatform.DataAccessLayer.Concrete
+{
+    public class ApplicationDbContext: IdentityDbContext<ApplicationUser>
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            // The base constructor handles initializing the DbContext with the provided options.
+        }
+
+    }
+}
